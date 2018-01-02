092942dfaab601b9e7dc83253b84dab07e16ca38
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2018-01-01 18:31:29 -0600

    Edit README.

---
+++ emotional-status-tracker/emotional-status-tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emotional-status-tracker
+//
+//  Created by Zachary Hardin on 1/1/18.
+//  Copyright © 2018 BlueFox Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
