f9bf350603f81bb463b4a9339c0f051a9002786b
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2018-01-19 17:36:11 -0600

    Add gesture recognizer when tapping stressed card.

--- emotional-status-tracker/emotional-status-tracker/ViewController.swift
+++ emotional-status-tracker/emotional-status-tracker/ViewController.swift
@@ -9,22 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var stressedViewContainer: UIView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        stressedViewContainer.layer.shadowRadius = 6
-//        stressedViewContainer.layer.shadowOffset = CGSize(width: 0, height: 1)
-//        stressedViewContainer.layer.shadowOpacity = 0.25
-//        stressedViewContainer.layer.shadowColor = UIColor.black.cgColor
-        
-
-//      When implimenting the process of selecting a card, you can increase
-//      the shadowOpacity to 0.75 and change the shadowColor to green.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func handleTap(_ sender: Any) {
+        print("tap")
     }
 }
